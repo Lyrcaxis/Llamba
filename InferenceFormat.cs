9c108f639aa218f411c6b6a50b9eb7ea236e0088
Lyrcaxis <lyrcaxis@gmail.com>
2024-10-20 05:03:59 +0300

    More tokenizers

--- InferenceFormat.cs
+++ InferenceFormat.cs
@@ -1,46 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 
 namespace Llamba {
-	public interface IInferenceFormat {
-		/// <summary> The begin-of-sequence text. </summary>
-		string BOS { get; }
+    public interface IInferenceFormat {
+        /// <summary> The begin-of-sequence text. </summary>
+        string BOS { get; }
 
-		/// <summary> The end-of-turn text. </summary>
-		string EOT { get; }
+        /// <summary> The end-of-turn text. </summary>
+        string EOT { get; }
 
-		/// <summary> Function that converts a message/turn into a string. </summary>
-		Func<ChatMessage, string> formatTransformFunc { get; }
+        /// <summary> Function that converts a message/turn into a string. </summary>
+        Func<ChatMessage, string> formatTransformFunc { get; }
 
-		/// <summary> Converts a collection of messages into a single string. </summary>
-		/// <remarks> Can optionally append the generation prompt marking the beginning of a new turn. </remarks>
-		public string TurnToString(IList<ChatMessage> messages, bool includeGenerationPrompt = true) {
-			var sb = new StringBuilder(BOS);
-			foreach (var msg in messages) { sb.Append(formatTransformFunc(msg)); }
-			if (includeGenerationPrompt) { sb.Append(formatTransformFunc(new("", Role.Assistant))); }
+        /// <summary> Converts a collection of messages into a single string. </summary>
+        /// <remarks> Can optionally append the generation prompt marking the beginning of a new turn. </remarks>
+        public string TurnToString(IList<ChatMessage> messages, bool includeGenerationPrompt = true) {
+            var sb = new StringBuilder(BOS);
+            foreach (var msg in messages) { sb.Append(formatTransformFunc(msg)); }
+            if (includeGenerationPrompt) { sb.Append(formatTransformFunc(new("", Role.Assistant))); }
+            //for (int i = 0; i < 10; i++) { Debug.WriteLine(""); } Debug.WriteLine(sb.ToString(0, sb.Length - EOT.Length).Replace("\r\n", "\n").TrimStart());
 
-			// Finally, return it without the last EOS, and with LF (Unix) line endings.
-			return sb.ToString(0, sb.Length - EOT.Length).Replace("\r\n", "\n").TrimStart();
-		}
-	}
+            // Finally, return it without the last EOS, and with LF (Unix) line endings.
+            return sb.ToString(0, sb.Length - EOT.Length).Replace("\r\n", "\n").TrimStart();
+        }
+    }
 
-	public class LLama3Format : IInferenceFormat {
-		public string BOS { get; init; } = "<|begin_of_text|>";
-		public string EOT { get; init; } = "<|eot_id|>";
-		public Func<ChatMessage, string> formatTransformFunc => (msg) => $"<|start_header_id|>{msg.role}<|end_header_id|>\n\n{msg.content}{EOT}";
+    public class LLama3Format : IInferenceFormat {
+        public string BOS { get; init; } = "<|begin_of_text|>";
+        public string EOT { get; init; } = "<|eot_id|>";
+        public Func<ChatMessage, string> formatTransformFunc => (msg) => $"<|start_header_id|>{msg.role}<|end_header_id|>\n\n{msg.content}{EOT}";
+    }
+    public class ChatMLFormat : IInferenceFormat {
+        public string BOS { get; init; } = "";
+        public string EOT { get; init; } = "<|im_end|>";
+        public Func<ChatMessage, string> formatTransformFunc => (msg) => $"\n<|im_start|>{msg.role}\n{msg.content}{EOT}";
+    }
+    public class MistralFormat : IInferenceFormat {
+        public string BOS { get; init; } = "<s>";
+        public string EOT { get; init; } = "</s>";
+        public Func<ChatMessage, string> formatTransformFunc => (msg) =>
+            msg.role switch {
+                "assistant" => $"[/INST] {msg.content}</s>",
+                _ => $"[INST] {msg.content}",
+            }; // This would actually be alright except the final space would be appended in the prompt.
 
-		public LLama3Format() { }
-		public LLama3Format(string EOT) => this.EOT = EOT;
-		public LLama3Format(string BOS, string EOT) => (this.BOS, this.EOT) = (BOS, EOT);
-	}
-	public class ChatMLFormat : IInferenceFormat {
-		public string BOS { get; init; } = "";
-		public string EOT { get; init; } = "<|im_end|>";
-		public Func<ChatMessage, string> formatTransformFunc => (msg) => $"\n<|im_start|>{msg.role}\n{msg.content}{EOT}";
+        /// <summary> Custom format transform for Mistral models, since they do not support system messages. </summary>
+        public string TurnToString(IList<ChatMessage> messages, bool includeGenerationPrompt = true) {
+            var sb = new StringBuilder(BOS);
 
-		public ChatMLFormat() { }
-		public ChatMLFormat(string EOT) => this.EOT = EOT;
-		public ChatMLFormat(string BOS, string EOT) => (this.BOS, this.EOT) = (BOS, EOT);
-	}
+            foreach (var m in messages) {
+                sb.Append(m.role switch {
+                    "assistant" => $"[/INST] {m.content.Trim()}</s>",
+                    _ => $"[INST] {m.content.Trim()}"
+                });
+            }
+
+            if (includeGenerationPrompt) { sb.Append($"[/INST]"); }
+            else if (sb.ToString().EndsWith(EOT)) { sb.Length -= EOT.Length; }
+            return sb.Replace("\r\n", "\n").Replace("\n ", "\n").Replace(" \n", "\n").ToString().TrimStart();
+        }
+    }
+
+    public class Gemma2Format : IInferenceFormat {
+        public string BOS { get; init; } = "<bos>";
+        public string BOT { get; init; } = "<start_of_turn>";
+        public string EOT { get; init; } = "<end_of_turn>";
+        public Func<ChatMessage, string> formatTransformFunc => throw new NotImplementedException();
+
+        /// <summary> Custom format transform for Gemma 2 models, since they do not support system messages. </summary>
+        public string TurnToString(IList<ChatMessage> messages, bool includeGenerationPrompt = true) {
+            var sb = new StringBuilder(BOS);
+
+            foreach (var m in messages) {
+                var (role, content, isSystem) = (m.role, m.content.Trim(), m.role == "system");
+                if (isSystem) { (role, content) = ("user", $"[SYSTEM:\n{content}\n]"); }
+                sb.Append($"\n{BOT}{role}\n{content}{EOT}");
+                if (isSystem) { sb.Append($"\n{BOT}assistant\n[Aknowledged.]{EOT}"); }
+            }
+
+            if (includeGenerationPrompt) { sb.Append($"\n{BOT}assistant\n"); }
+            else if (sb.ToString().EndsWith(EOT)) { sb.Length -= EOT.Length; }
+
+            sb = sb.Replace(">assistant", ">model").Replace(">next", ">system").Replace("<bos>\n", "<bos>");
+            return sb.Replace("\r\n", "\n").Replace("\n ", "\n").Replace(" \n", "\n").ToString().TrimStart();
+        }
+    }
+
 }
--- Tokenization/Tokenizer.cs
+++ Tokenization/Tokenizer.cs
@@ -7,59 +7,65 @@ using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Llamba.Tokenization {
-	/// <summary>
-	/// A C# wrapper for huggingface's python tokenizer library. Can encode and decode <b>(tokens -> string -> tokens)</b> fluently on top-tier level.
-	/// <para> Internally hosts a python server and the tokenization happens via localhost endpoint <b>requests -> responses</b>. See `tokenization.py`. </para>
-	/// <para> This is slower than native encoding/decoding because of the serialization overhead, but fast enough for any and all purposes. </para>
-	/// </summary>
-	public class Tokenizer {
+    /// <summary>
+    /// A C# wrapper for huggingface's python tokenizer library. Can encode and decode <b>(tokens -> string -> tokens)</b> fluently on top-tier level.
+    /// <para> Internally hosts a python server and the tokenization happens via localhost endpoint <b>requests -> responses</b>. See `tokenization.py`. </para>
+    /// <para> This is slower than native encoding/decoding because of the serialization overhead, but fast enough for any and all purposes. </para>
+    /// </summary>
+    public class Tokenizer {
+        static HttpClient client = new() { Timeout = TimeSpan.FromSeconds(5) };
+        static Process currentServerProcess;
+        const string hostIP = "http://127.0.0.1:8150";
 
-		static HttpClient client = new() { Timeout = TimeSpan.FromSeconds(5) };
-		static Process currentServerProcess;
-		const string hostIP = "http://127.0.0.1:8150";
+        static Tokenizer() {
+            if (Model.instance.format is LLama3Format) { InitializeTokenizer("Tokenization/llama-3-tokenizer.json"); }
+            else if (Model.instance.format is Gemma2Format) { InitializeTokenizer("Tokenization/gemma-2-tokenizer.json"); }
+            else if (Model.instance.format is MistralFormat) {
+                if (Model.vocab.Count > 130000) { InitializeTokenizer("Tokenization/mistral-nemo-tokenizer.json"); }
+                else { InitializeTokenizer("Tokenization/mistral-tokenizer.json"); }
+            }
+        }
 
-		static Tokenizer() => InitializeTokenizer("Tokenization/llama-3-tokenizer.json");
+        /// <summary> Load the tokenizer from the specified path. This internally creates the python server that hosts the tokenizer. </summary>
+        /// <remarks> This method should be called each time a new tokenizer should be loaded. </remarks>
+        public static void InitializeTokenizer(string tokenizerPath) {
+            if (currentServerProcess != null) { currentServerProcess?.Kill(); }
+            Console.WriteLine($"Starting python server at {hostIP} -- this will act as the tokenizer.");
+            currentServerProcess = new Process { StartInfo = new("python", $"Tokenization/tokenization.py --path {tokenizerPath}") { UseShellExecute = false, CreateNoWindow = true } };
+            currentServerProcess.Start();
 
-		/// <summary> Load the tokenizer from the specified path. This internally creates the python server that hosts the tokenizer. </summary>
-		/// <remarks> This method should be called each time a new tokenizer should be loaded. </remarks>
-		public static void InitializeTokenizer(string tokenizerPath) {
-			if (currentServerProcess != null) { currentServerProcess?.Kill(); }
-			Console.WriteLine($"Starting python server at {hostIP} -- this will act as the tokenizer.");
-			currentServerProcess = new Process { StartInfo = new("python", $"Tokenization/tokenization.py --path {tokenizerPath}") { UseShellExecute = false, CreateNoWindow = true } };
-			currentServerProcess.Start();
-
-			// Make sure the python application exits together with the server. NOTE: This will not occur when pressing 'STOP' from VS.
-			AppDomain.CurrentDomain.ProcessExit += (s, e) => { if (!currentServerProcess.HasExited) { currentServerProcess.Kill(); } };
-			var curProccess = Process.GetCurrentProcess(); curProccess.EnableRaisingEvents = true; curProccess.Exited += (_, _) => currentServerProcess.Kill();
-		}
+            // Make sure the python application exits together with the server.
+            AppDomain.CurrentDomain.ProcessExit += (s, e) => { if (!currentServerProcess.HasExited) { currentServerProcess.Kill(); } };
+            var curProccess = Process.GetCurrentProcess(); curProccess.EnableRaisingEvents = true; curProccess.Exited += (_, _) => currentServerProcess.Kill();
+        }
 
 
-		/// <summary> Turns the string into a sequence of tokens. </summary>
-		public static List<int> Encode(string text) => EncodeAsync(text).Result;
+        /// <summary> Turns the string into a sequence of tokens. </summary>
+        public static List<int> Encode(string text) => EncodeAsync(text).Result;
 
-		/// <summary> Turns the sequence of tokens into a string. </summary>
-		public static string Decode(List<int> tokens) => DecodeAsync(tokens).Result;
+        /// <summary> Turns the sequence of tokens into a string. </summary>
+        public static string Decode(List<int> tokens) => DecodeAsync(tokens).Result;
 
-		/// <summary> Turns the string into a sequence of tokens. </summary>
-		public async static Task<List<int>> EncodeAsync(string text) {
-			var response = "";
-			try {
-				var message = new HttpRequestMessage(HttpMethod.Post, $"{hostIP}/encode") { Content = JsonContent.Create(new { text }) };
-				using var httpResponse = await client.SendAsync(message);
-				return JsonSerializer.Deserialize<List<int>>(response = await httpResponse.Content.ReadAsStringAsync());
-			}
-			catch { Debug.WriteLine($"Request failed: {response}"); return []; }
-		}
+        /// <summary> Turns the string into a sequence of tokens. </summary>
+        public async static Task<List<int>> EncodeAsync(string text) {
+            var response = "";
+            try {
+                var message = new HttpRequestMessage(HttpMethod.Post, $"{hostIP}/encode") { Content = JsonContent.Create(new { text }) };
+                using var httpResponse = await client.SendAsync(message);
+                return JsonSerializer.Deserialize<List<int>>(response = await httpResponse.Content.ReadAsStringAsync());
+            }
+            catch { Debug.WriteLine($"Request failed: {response}"); return []; }
+        }
 
-		/// <summary> Turns the sequence of tokens into a string. </summary>
-		public async static Task<string> DecodeAsync(List<int> tokens) {
-			var response = "";
-			try {
-				var message = new HttpRequestMessage(HttpMethod.Post, $"{hostIP}/decode") { Content = JsonContent.Create(new { tokens }) };
-				using var httpResponse = await client.SendAsync(message);
-				return (response = await httpResponse.Content.ReadAsStringAsync())[1..^1]; // Quicker `Deserialize<string>(..)`
-			}
-			catch { Debug.WriteLine($"Request failed: {response}"); return ""; }
-		}
-	}
+        /// <summary> Turns the sequence of tokens into a string. </summary>
+        public async static Task<string> DecodeAsync(List<int> tokens) {
+            var response = "";
+            try {
+                var message = new HttpRequestMessage(HttpMethod.Post, $"{hostIP}/decode") { Content = JsonContent.Create(new { tokens }) };
+                using var httpResponse = await client.SendAsync(message);
+                return (response = await httpResponse.Content.ReadAsStringAsync())[1..^1]; // Quicker `Deserialize<string>(..)`
+            }
+            catch { Debug.WriteLine($"Request failed: {response}"); return ""; }
+        }
+    }
 }
